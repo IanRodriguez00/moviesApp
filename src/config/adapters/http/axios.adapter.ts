f4b1b45b152caa02f41d384c29f87ffc54f8ac50
IanRodriguez00 <girm.00@hotmail.com>
2024-08-06 00:22:48 -0600

    adaptador axios

---
+++ src/config/adapters/http/axios.adapter.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance } from 'axios';
+import {httpAdapter} from './http.adapter';
+
+interface Options {
+    baseUrl: string;
+    params: Record<string,string>
+}
+
+export class AxiosAdapter implements httpAdapter {
+
+    private axiosInstance: AxiosInstance;
+
+    constructor(options: Options) {
+        this.axiosInstance = axios.create({
+            baseURL: options.baseUrl,
+            params: options.params,
+        });
+    }
+
+  async get<T>(url: string,options?: Record <string, unknown> | undefined ): Promise<T> {
+    try {
+
+        const { data } = await axios.get<T>(url,options);
+        return data;
+
+    } catch (error) {
+        throw new Error(`Error fetching get: ${url}`);
+    }
+  }
+}
