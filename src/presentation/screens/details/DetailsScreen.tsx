dd826569e3a1be8c4b2b0557b3cd196135713c2a
IanRodriguez00 <girm.00@hotmail.com>
2024-08-15 02:02:34 -0600

    Header de movie y inicio de detalles movie

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,8 +1,11 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import { RootStackParams } from '../../navigation/Navigation';
 import { useMovie } from '../../hooks/useMovie';
+import { MovieHeader } from '../../components/movie/MovieHeader';
+import { MovieDetails } from '../../components/movie/MovieDetails';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 interface Props extends StackScreenProps<RootStackParams,'Details'>{}
@@ -13,13 +16,23 @@ export const DetailsScreen = ({route}: Props) => {
   const { movieId } = route.params;
 
   //console.log({movieId});
-  const {} = useMovie(movieId);
+  const {isLoading, movie } = useMovie(movieId);
+
+  if(isLoading){
+    return <Text>Loading</Text>;
+  }
 
   return (
-    <View>
+    <ScrollView>
    {/*Header */}
-
+    <MovieHeader
+    poster={ movie!.poster }
+    originalTitle={ movie!.originalTitle }
+    title={ movie!.title }
+    />
    {/*Details */}
-    </View>
+   <MovieDetails
+   movie={movie!}/>
+    </ScrollView>
   );
 };
